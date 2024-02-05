f0f30717014bb41b6b59f6609229c0f1641f1908
RickDelgado <ra.delgadonava@gmail.com>
2024-02-05 11:26:23 -0600

    fin estructura

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.mmodule';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 
 
 @NgModule({
   declarations: [
-    LayoutPageComponent
+    LayoutPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
   ],
   imports: [
     CommonModule,
