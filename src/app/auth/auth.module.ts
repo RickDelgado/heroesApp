9a7047f946c795ad249785ab2cc3844a7b06429e
RickDelgado <ra.delgadonava@gmail.com>
2024-02-05 11:15:14 -0600

    primer commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from './auth-routing.mmodule';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    LayoutPageComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
