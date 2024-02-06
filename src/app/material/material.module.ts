c153dca4fadcce4b8565ef55c430aa19dd7d349a
RickDelgado <ra.delgadonava@gmail.com>
2024-02-05 22:36:14 -0600

    instalación material y prime

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete'; 
+import { MatButtonModule } from '@angular/material/button'; 
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatAutocompleteModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
